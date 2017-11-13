9712418eb79d967769063c71e9aca8c699b41aa1
Michelle Brien <michelle.brien12@gmail.com>
2017-11-13 10:47:42 -0200

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsStupidCoaching::Application.config.secret_token = '497037c8763845ebc37a56d07258eee2a574a20cf8681cf372564285b530454fbf92ba1a76a4b0b191597d600f9ea1599d71f89689b8e5e5511065f9c02b3953'
